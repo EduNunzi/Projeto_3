96966a2b45319a45edc56e41941caf777a635b15
EduNunzi <148161063+EduNunzi@users.noreply.github.com>
2023-11-12 19:18:44 -0300

    Implementação da função de exportação, que valida só por prioridade, que exporta para um txt todas as tarefas que tem a mesma prioridade em comum.

--- funcoes.c
+++ funcoes.c
@@ -325,3 +325,36 @@ void Filtrar_Por_Prioridade_E_Categoria(struct STarefas tarefas[], int numero_de
         printf("Nao ha tarefas com a prioridade %d e categoria %s",Prioridade_da_Tarefa, Categoria_da_Tarefa);
     }
 }
+
+void Exportar_Por_Prioridade(struct STarefas tarefas[], int numero_de_tarefas) {
+    int Prioridade_da_Tarefa;
+    printf("Digite a prioridade que deseja filtrar: ");
+    scanf("%d", &Prioridade_da_Tarefa);
+
+    FILE *arquivo = fopen("Tarefas_Prioridade.txt", "w");
+    if (arquivo == NULL) {
+        printf("Erro ao abrir o arquivo de tarefas por prioridade para escrita.\n");
+        return;
+    }
+
+    int Exportacao_de_Tarefas = 0;
+
+    for (int i = 0; i < numero_de_tarefas; i++) {
+        if (tarefas[i].prioridade == Prioridade_da_Tarefa) {
+            fprintf(arquivo, "Prioridade: %d\n", tarefas[i].prioridade);
+            fprintf(arquivo, "Descricao: %s", tarefas[i].descricao);
+            fprintf(arquivo, "Categoria: %s", tarefas[i].categoria);
+            fprintf(arquivo, "Estado: %s\n", tarefas[i].estado);
+            fprintf(arquivo, "\n");
+            Exportacao_de_Tarefas++;
+        }
+    }
+
+    fclose(arquivo);
+
+    if (Exportacao_de_Tarefas > 0) {
+        printf("Exportacao conclida. O arquivo 'Tarefas_Prioridade.txt' foi gerado.\n", Prioridade_da_Tarefa);
+    } else {
+        printf("Nao ha tarefas com a prioridade %d para exportar.\n", Prioridade_da_Tarefa);
+    }
+}
