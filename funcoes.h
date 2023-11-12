27c915c0d5df7644a221e72b92f566e7b70f2c40
EduNunzi <148161063+EduNunzi@users.noreply.github.com>
2023-11-12 18:59:51 -0300

    Adição dos nomes da variaveis e seus parametros na funções.h.

--- funcoes.h
+++ funcoes.h
@@ -3,18 +3,38 @@
 #define PROJETO_TAREFAS_FUNCOES_H
 
 #endif //PROJETO_TAREFAS_FUNCOES_H
-typedef struct {
+
+#define MAX_TAREFAS 100
+
+typedef struct STarefas{
     int prioridade;
     char descricao[300];
     char categoria[100];
-}STarefas;
+    char estado[20];
+};
 
-int Salvar_Tarefas(STarefas tarefas[], int numero_de_tarefas);
+int Salvar_Tarefas(struct STarefas tarefas[], int numero_de_tarefas);
 
-int Carregar_Tarefas(STarefas tarefas[]);
+int Carregar_Tarefas(struct STarefas tarefas[]);
 
-int Cadastrar_Tarefas(STarefas tarefas[], int *numero_de_tarefas);
+int Cadastrar_Tarefas(struct STarefas tarefas[], int *numero_de_tarefas);
 
-int Listar_Tarefas(STarefas tarefas[], int numero_de_tarefas);
+int Listar_Tarefas(struct STarefas tarefas[], int numero_de_tarefas);
 
-int Deletar_Tarefa(STarefas tarefas[], int *numero_de_tarefas, char cate[], int priori);
+int Deletar_Tarefa(struct STarefas tarefas[], int *numero_de_tarefas, char cate[], int priori);
+
+void Alterar_Tarefa(struct STarefas tarefas[], int numero_de_tarefas);
+
+void Filtrar_Por_Prioridade(struct STarefas tarefas[], int numero_de_tarefas);
+
+void Filtrar_Por_Estado(struct STarefas tarefas[], int numero_de_tarefas);
+
+void Filtrar_Por_Categoria(struct STarefas tarefas[], int numero_de_tarefas);
+
+void Filtrar_Por_Prioridade_E_Categoria(struct STarefas tarefas[], int numero_de_tarefas);
+
+void Exportar_Por_Prioridade(struct STarefas tarefas[], int numero_de_tarefas);
+
+void Exportar_Por_Categoria(struct STarefas tarefas[], int numero_de_tarefas);
+
+void Exportar_Por_Prioridade_E_Categoria(struct STarefas tarefas[], int numero_de_tarefas);
